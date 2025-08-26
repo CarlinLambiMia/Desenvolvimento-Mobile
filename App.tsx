e47b53547ad9d0dfc771b6e41298160ae2d4f21b
CarlinLambiMia <cego.judo@gmail.com>
2025-08-26 10:07:01 -0400

    atualização

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Projeto de desenvolvimento mobile</Text>
+      <TextInput style={styles.campo} placeholder='Digite se confirma' placeholderTextColor="red"/>
+      <Button title="Botão" onPress={() =>console.log("Teste")} color={'#005f5f'}/>
+      <TouchableOpacity style={styles.button} activeOpacity={0.6} onPress={()=>console.log("Teste de botão")}>
+        <Text style={styles.buttonText}>Botão teste</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,6 +19,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  
     alignItems: 'center',
     justifyContent: 'center',
   },
